0008e39f3ea53b14678b2fb5681f3a4843d3385d
Ramuthakor <67890773+Ramuthakor@users.noreply.github.com>
2023-07-23 11:34:37 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "ramucalif.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "raj.com";
+accountPassword = "007008"
+accountCity = "Bengaluru"
+
+let accountState 
+
+// accountId = 2 not allowed 
+
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState]);
+
+/*
+Prefer not to use var
+beause of issue in block scope and functional scope
+*/
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strick"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using node js, not browser
+
+// number 
+//bigint
+// string =""
+// boolean => true/false
+// null => standalone value // null is an object type
+// undefined // its an undefined type
+// symbol => unique
+
+
+//object
+
